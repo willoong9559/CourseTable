08e1667545c261b5f36b680b3c8dddee9cc058b7
willoong9559 <501801418@qq.com>
2019-07-23 09:14:23 +0800

    引入litepal,创建Course数据库

---
+++ app/src/main/java/com/example/coursetable/Course.java
@@ -0,0 +1,57 @@
+package com.example.coursetable;
+
+public class Course {
+
+    private String coursename;
+
+    private int course_start;
+
+    private int course_end;
+
+    private int classroom;
+
+    private String teacher_name;
+
+    public String getCoursename() {
+        return coursename;
+    }
+
+    public void setCoursename(String coursename) {
+        this.coursename = coursename;
+    }
+
+    public int getCourse_start() {
+        return course_start;
+    }
+
+    public void setCourse_start(int course_start) {
+        this.course_start = course_start;
+    }
+
+    public int getCourse_end() {
+        return course_end;
+    }
+
+    public void setCourse_end(int course_end) {
+        this.course_end = course_end;
+    }
+
+    public int getClassroom() {
+        return classroom;
+    }
+
+    public void setClassroom(int classroom) {
+        this.classroom = classroom;
+    }
+
+    public String getTeacher_name() {
+        return teacher_name;
+    }
+
+    public void setTeacher_name(String teacher_name) {
+        this.teacher_name = teacher_name;
+    }
+
+}
+
+
